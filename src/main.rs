4659393d31beef70458e21cf0e8604841ecd787b
Cody P Schafer <dev@codyps.com>
2016-04-14 11:06:20 -0400

    initial

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(feature = "serde_macros", feature(custom_derive, plugin))]
+#![cfg_attr(feature = "serde_macros", plugin(serde_macros))]
+
+#[macro_use]
+extern crate log;
+
+extern crate serde;
+extern crate serde_json;
+
+#[cfg(feature = "serde_macros")]
+include!("main.rs.in");
+
+#[cfg(not(feature = "serde_macros"))]
+include!(concat!(env!("OUT_DIR"), "/main.rs"));
